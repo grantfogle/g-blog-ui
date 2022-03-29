67d76202fbd0394b2dbf2248f297c8abccd1f936
grantfogle <grantjfogle@gmail.com>
2022-03-29 16:28:14 -0600

    setting up basic layout for app

---
+++ components/footer.js
@@ -0,0 +1,8 @@
+import Box from '@mui/material/Box';
+
+export default function Footer({ children }) {
+    return (
+        <Box color="dark" sx={{ height: '200px', width: '100%', backgroundColor: '#2c3e50' }}>
+        </Box>
+    )
+}
