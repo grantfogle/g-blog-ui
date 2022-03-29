fef25fd9dedb2c6dfd8ef3b383581cdf76bedfe7
grantfogle <grantjfogle@gmail.com>
2022-03-29 16:33:37 -0600

    adding basic styling for layout of header and foooter

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,14 @@
+import Box from '@mui/material/Box';
 import Navbar from './navbar'
 import Footer from './footer'
+import { flexbox } from '@mui/system';
 
 export default function Layout({ children }) {
     return (
-        <>
+        <Box sx={{ display: 'flex', minHeight: '100vh', flexDirection: 'column', justifyContent: 'space-between' }}>
             <Navbar />
             <main>{children}</main>
             <Footer />
-        </>
+        </Box>
     )
 }
