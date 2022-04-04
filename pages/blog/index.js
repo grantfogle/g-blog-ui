4bcd30155d21c89e7f78e0cea737789b1ea4e23b
grantfogle <grantjfogle@gmail.com>
2022-04-03 19:47:34 -0600

    adding formatted date

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,15 +1,24 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Link from 'next/link'
-import { getAllPostsForHome } from "../lib/api";
+import { getAllPostsForHome } from "../../lib/api";
+
+
+function formatDates(unforDate) {      
+    const newDate = new Date(unforDate);
+    return `${newDate.getMonth() + 1}/${newDate.getDate()}/${newDate.getFullYear()}`;
+};
+
+
 
 export default function Blog({ preview, allPosts }) {
-    console.log(preview, allPosts)
+
     return (
         <Box sx={{ width: '600px', height: '100%', p: 4, margin: '0 auto' }}>
             {allPosts.map(post => {
                 return (
                     <div key={post.sys.id}>
+                        <p>{formatDates(post.date)}</p>
                         <Link href={`/blog/ + ${post.title}`}>
                             <h1>{post.title}</h1>
                         </Link>
