3c7e00948ea6268b365011378b44e4e1f33afde2
grantfogle <grantjfogle@gmail.com>
2022-04-04 22:22:45 -0600

    this attempt is waaayyyyyy better

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,51 @@ import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 
-export default function Index() {
+let client = require('contentful').createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+});
+
+export async function getStaticProps() {
+  let data = await client.getEntries({
+    content_type: 'blogPost',
+  })  
+  console.log('bing')
+  return {
+    props: {
+      allPosts: data.items
+    }
+  }
+}
+
+function formatDates(unforDate) {
+  const newDate = new Date(unforDate);
+  return `${newDate.getMonth() + 1}/${newDate.getDate()}/${newDate.getFullYear()}`;
+};
+
+export default function Index({allPosts}) {
+  console.log(allPosts)
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
+        <div>
+        {allPosts.map(post => {
+          return (
+            <Link href={"/blog/" + post.fields.slug}>
+
+              <h4>{formatDates(post.fields.date)} {post.fields.title}</h4>
+            </Link>
+          )
+        })}
+        </div>
+        {/* <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
         <ProTip />
-        <Copyright />
+        <Copyright /> */}
       </Box>
     </Container>
   );
